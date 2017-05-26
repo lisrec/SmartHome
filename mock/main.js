5bb09d2ac7ae7bf55f79f3d35fa80b6a203252be
Przemysław Jóźwik <s407222@pc25h.labs.wmi.amu.edu.pl>
2017-05-26 11:37:38 +0200

    frontend mock

---
+++ mock/main.js
@@ -0,0 +1,18 @@
+function b1(){
+	console.log("some");
+};
+function b2(){
+	console.log("some");
+};
+function b3(){
+	console.log("some");
+};
+function b4(){
+	console.log("some");
+};
+function b5(){
+	console.log("some");
+};
+function b6(){
+	console.log("some");
+};
