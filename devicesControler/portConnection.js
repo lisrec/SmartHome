1b3630e1e03ac97e1acae6a0204cde503253dbac
mahizz <juzioo94@gmail.com>
2017-11-12 18:56:23 +0100

    change to promises

--- devicesControler/portConnection.js
+++ devicesControler/portConnection.js
@@ -1,21 +1,27 @@
+const  Promise = require('bluebird')
 
 const initArduino = function ( SerialPort  ) {
-	SerialPort.list(function (err, ports) {
-		if(!err) {
-			console.log(ports)
-			ports.forEach((device) => {
-				if(device.manufacturer && device.manufacturer.toLowerCase().indexOf("arduino") !== -1) {
-					port = new SerialPort(device.comName, {
-						parser: SerialPort.parsers.readline('\n')
-					})
-					port.on('data', function (data) {
-						console.log('Data: ' + data)
-					})
-				}
-			})
-		} else {
-			console.log("Cannot load SerialPort.")
-		}
+	return new Promise( (resolve, reject) => {
+		SerialPort.list( (err, ports) => {
+			if(!err) {
+				ports.forEach((device) => {
+					if(device.manufacturer && device.manufacturer.toLowerCase().indexOf("arduino") !== -1) {
+						port = new SerialPort(device.comName, {
+							parser: SerialPort.parsers.readline('\n')
+						})
+							resolve(port)
+					}
+				})
+			} else {
+				reject("Cannot load SerialPort.")
+			}
+		})
+	})
+}
+
+const deinitArduino = function( port ) => {
+	port.close(() => {
+		callback(true)
 	})
 }
 
