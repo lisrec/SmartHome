9f9bc12bb28461749aa8ab22fbda059c96ec2c7c
Unknown <przemsylaw.jozwik@aegton.com>
2017-11-28 11:10:05 +0100

    users resource

--- server/models/users.js
+++ server/models/users.js
@@ -2,10 +2,25 @@ module.exports = (sequelize, Sequelize) => {
 
 	const User = sequelize.define('user', {
 		login: {
-			type: Sequelize.STRING
+			type: Sequelize.STRING,
+			unique: true,
+			allowNull: false,
+			validate: {
+				len: [3,30]
+			}
 		},
 		pass: {
-			type: Sequelize.STRING
+			type: Sequelize.STRING(128),
+			allowNull: false,
+			validate: {
+				len: [3,128]
+			}
+		},
+		imie: {
+			type: Sequelize.STRING(30)
+		},
+		nazwisko: {
+			type: Sequelize.STRING(30)
 		}
 	}, {
 		timestamps: false
